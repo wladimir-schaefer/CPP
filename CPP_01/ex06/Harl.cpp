fff83a7c06ca03158f77adc4590f983e78542cf5
wschafer <wschafer@student.42berlin.de>
2026-04-06 10:15:29 +0200

    Refactor: Remove unnecessary comments and improve code readability in Zombie and main files; add Harl class implementation with complaint handling

--- CPP_01/ex06/Harl.cpp
+++ CPP_01/ex06/Harl.cpp
@@ -1,41 +1,48 @@
 #include "Harl.hpp"
 
 void Harl::debug( void ){
-	std::cout << 
+	std::cout << "[ DEBUG ]" << std::endl <<
 	"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger."
 	<< " I really do!" 
-	<< std::endl;
+	<< std::endl << std::endl;
 }
 void Harl::info( void ){
-	std::cout << 
+	std::cout << "[ INFO ]" << std::endl <<
 	"I cannot believe adding extra bacon costs more money. "
 	<< "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!"
-	<< std::endl;
+	<< std::endl << std::endl;
 }
 void Harl::warning( void ){
-	std::cout << 
+	std::cout << "[ WARNING ]" << std::endl <<
 	"I think I deserve to have some extra bacon for free. I’ve been coming for"
 	<< "years, whereas you started working here just last month." 
-	<< std::endl;
+	<< std::endl << std::endl;
 }
 void Harl::error( void ){
-	std::cout << 
+	std::cout << "[ ERROR ]" << std::endl <<
 	"This is unacceptable! I want to speak to the manager now." 
-	<< std::endl;
+	<< std::endl << std::endl;
 }
 
 void Harl::complain( std::string level ){
-	void (Harl::*f[])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 	std::string type[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 	int i = 0;
-	while (i < 4)
-	{
+	while (i < 4){
 		if (type[i] == level)
-		{
-			(this->*f[i])();
-			return ;
-		}
+			break;
 		i++;
 	}
-	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	switch (i){
+		case 0:
+			this->debug();
+		case 1:
+			this->info();
+		case 2:
+			this->warning();
+		case 3:
+			this->error();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 }
