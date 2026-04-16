333a8c8eb2083aa33461f067bf1a0ba3645c9e29
wschafer <wschafer@student.42berlin.de>
2026-04-16 17:06:41 +0200

    Implement Fixed class constructors for int and float; update main function for enhanced testing

--- CPP_02/ex01/Fixed.hpp
+++ CPP_02/ex01/Fixed.hpp
@@ -2,6 +2,7 @@
 # define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
 
 class Fixed{
 
@@ -11,11 +12,20 @@ private:
 
 public:
 	Fixed();
+	Fixed(const int num);
+	Fixed(const float num);
 	Fixed (const Fixed &other);
 	Fixed &operator=(const Fixed &other);
 	~Fixed();
 	int getRawBits( void ) const;
 	void setRawBits( int const raw );
+	float toFloat( void ) const;
+	int toInt( void ) const;
 };
+
+std::ostream &operator<<(std::ostream &out, const Fixed &obj) {
+	out << obj.toFloat();
+	return out;
+}
 
 #endif
